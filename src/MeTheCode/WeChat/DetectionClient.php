48cf9d26ad165c39561097b07e1c745d29e4be94
methecode <methecodez@gmail.com>
2023-02-07 00:24:57 +0800

    update

--- src/MeTheCode/WeChat/DetectionClient.php
+++ src/MeTheCode/WeChat/DetectionClient.php
@@ -31,6 +31,22 @@ class DetectionClient
 		$this->appCode    = $appCode;
 	}
 	
+	protected function createDefaultHeaders ()
+	{
+		return [
+			'Authorization' => 'AppCode/' . $this->appCode
+		];
+	}
+	
+	/**
+	 * 参考：https://91.zhuolianai.com/item/2
+	 *
+	 * @param $url
+	 * @param $type
+	 * @return mixed
+	 * @author  methecode
+	 * @date    2023/2/7 00:21
+	 */
 	public function urlCheck ( $url , $type = self::URL_CHECK_TYPE_WECHAT )
 	{
 		$headers = $this->createDefaultHeaders();
@@ -51,11 +67,31 @@ class DetectionClient
 		return $respJson['data'];
 	}
 	
-	private function createDefaultHeaders ()
+	/**
+	 * 参考：https://91.zhuolianai.com/item/1
+	 *
+	 * @param $appId
+	 * @return mixed
+	 * @author  hongyongzhu
+	 * @date    2023/2/7 00:23
+	 */
+	public function queryWechatMpStatus ( $appId )
 	{
-		return [
-			'Authorization' => 'AppCode/'. $this->appCode
-		];
+		$headers = $this->createDefaultHeaders();
+		$resp    = $this->httpClient->request( 'GET' , self::REQUEST_URL . "/91/v1/wechat/mp/status" , [
+			'appId' => $appId
+		] , $headers );
+		
+		if ( $resp['http_code'] != 200 ) {
+			throw new DetectionException( "Request failed: {$resp['http_code']} - " . $resp['response'] , -2 );
+		}
+		
+		$respJson = json_decode( $resp['response'] , true );
+		if ( $respJson['errCode'] !== 0 ) {
+			throw new DetectionException( "Invoke failed: " . $respJson['errMsg'] , $respJson['errCode'] );
+		}
+		
+		return $respJson['data'];
 	}
 	
 }
